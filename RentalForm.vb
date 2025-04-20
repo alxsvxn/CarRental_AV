90a96d20702befcba9052776f5d432e04ede1d57
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-04-20 15:38:16 -0600

    Set Default values

--- RentalForm.vb
+++ RentalForm.vb
@@ -1,6 +1,29 @@
-﻿Option Explicit On
+﻿'Alexis Villagran
+'RCET
+'Spring 2025
+
+Option Explicit On
 Option Strict On
 Option Compare Binary
 Public Class RentalForm
+	Sub SetDefaults()
+		NameTextBox.Text = ""
+		AddressTextBox.Text = ""
+		CityTextBox.Text = ""
+		StateTextBox.Text = ""
+		ZipCodeTextBox.Text = ""
+		BeginOdometerTextBox.Text = ""
+		EndOdometerTextBox.Text = ""
+		DaysTextBox.Text = ""
+		TotalMilesTextBox.Text = ""
+		MileageChargeTextBox.Text = ""
+		DayChargeTextBox.Text = ""
+		TotalDiscountTextBox.Text = ""
+		TotalChargeTextBox.Text = ""
+		AAAcheckbox.Checked = False
+		Seniorcheckbox.Checked = False
+		MilesradioButton.Checked = True
+		NameTextBox.Focus()
+	End Sub
 
 End Class
