66f414be0a9d4d8bc26d230b25263aea1d7219dc
Alexis Villagran <alexisvillagran18@hotmail.com>
2025-04-20 18:16:15 -0600

    Added input validation for textboxes

--- RentalForm.vb
+++ RentalForm.vb
@@ -25,5 +25,63 @@ Public Class RentalForm
 		MilesradioButton.Checked = True
 		NameTextBox.Focus()
 	End Sub
+	Function InputsAreValid() As Boolean
+		Dim message As String = ""
+		Dim valid As Boolean = True
+
+		If NameTextBox.Text = "" Then
+			message += "Customer Name is required." & vbCrLf
+			valid = False
+		End If
+		If AddressTextBox.Text = "" Then
+			message += "Address is required." & vbCrLf
+			valid = False
+		End If
+		If CityTextBox.Text = "" Then
+			message += "City is required." & vbCrLf
+			valid = False
+		End If
+		If StateTextBox.Text = "" Then
+			message += "State is required." & vbCrLf
+			valid = False
+		End If
+		If ZipCodeTextBox.Text = "" Then
+			message += "Zip Code is required." & vbCrLf
+			valid = False
+		End If
+
+		Dim beginOdometer As Double
+		If Not Double.TryParse(BeginOdometerTextBox.Text, beginOdometer) Then
+			message += "Must be a number." & vbCrLf
+			valid = False
+		End If
+
+		Dim endOdometer As Double
+		If Not Double.TryParse(EndOdometerTextBox.Text, endOdometer) Then
+			message += "Must be a number." & vbCrLf
+			valid = False
+		End If
+
+		If valid AndAlso beginOdometer >= endOdometer Then
+			message += "Beginning Odometer must be less than Ending Odometer." & vbCrLf
+			valid = False
+		End If
+
+		Dim days As Integer
+		If Not Integer.TryParse(DaysTextBox.Text, days) Then
+			message += "Number of Days must be a whole number." & vbCrLf
+			valid = False
+		ElseIf days <= 0 OrElse days > 45 Then
+			message += "Number of Days must be between 1 and 45." & vbCrLf
+			valid = False
+		End If
+
+		If Not valid Then
+			MsgBox(message)
+		End If
+
+		Return valid
+	End Function
+
 
 End Class
